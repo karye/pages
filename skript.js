b83e2cd0976e7f224f1330504f7b76e51f5dec4e
karye <karye.webb@gmail.com>
2024-05-30 08:40:30 +0200

    Nytt

---
+++ skript.js
@@ -0,0 +1,4 @@
+    document.querySelector('button').addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+    document.querySelector('.kontainer').classList.toggle('dark');
+});
